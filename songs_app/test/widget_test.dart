c989d15ebcb6790cdb1321e9238c2961facb38e0
Bruno Siqueira <brunovsiq@gmail.com>
2023-09-17 14:04:32 -0300

    add ProviderScope as root widget

--- songs_app/test/widget_test.dart
+++ songs_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:songs_app/main.dart';
+import 'package:songs_app/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
